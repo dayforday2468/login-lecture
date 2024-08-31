c93ee0deb9c722ef7c0dea30797b35249b80d2be
dayforday2468 <dayforday@kaist.ac.kr>
2024-08-31 17:20:54 +0900

    벡엔드에서 로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -11,6 +11,9 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 app.use(express.static(`${__dirname}/src/public`));
+app.use(express.json());
+//url을 이용하여 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식하지 않는 문제 해결
+app.use(express.urlencoded({ extended: true }));
 app.use("/", home)
 
 module.exports = app;
