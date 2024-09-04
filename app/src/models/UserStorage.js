ebe53ae333043a952629c024b266c43770655ba8
dayforday2468 <dayforday@kaist.ac.kr>
2024-09-04 19:14:50 +0900

    fs모듈을 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,25 +1,11 @@
 "use strict"
 
+const fs = require("fs").promises;
+
 class UserStorage {
-  static #users = {
-    id: ["woorimIT", "나개발", "김팀장"],
-    psword: ["1234", "1234", "123456"],
-    name: ["우리밋", "나개발", "김팀장"]
-  };
 
-  static getUsers(...fields) {
-    const users = this.#users;
-    const newUsers = fields.reduce((newUsers, field) => {
-      if (users.hasOwnProperty(field)) {
-        newUsers[field] = users[field];
-      }
-      return newUsers;
-    }, {});
-    return newUsers;
-  };
-
-  static getUserInfo(id) {
-    const users = this.#users;
+  static #getUserInfo(data, id) {
+    const users = JSON.parse(data);
     const idx = users.id.indexOf(id);
     const userInfo = Object.keys(users).reduce((newUser, info) => {
       newUser[info] = users[info][idx];
@@ -28,12 +14,37 @@ class UserStorage {
     return userInfo;
   };
 
+  static getUsers(...fields) {
+    fs.readFile("./src/databases/users.json", (err, data) => {
+      if (err) throw err;
+      const users = JSON.parse(data);
+      const newUsers = fields.reduce((newUsers, field) => {
+        if (users.hasOwnProperty(field)) {
+          newUsers[field] = users[field];
+        }
+        return newUsers;
+      }, {});
+      return newUsers;
+    })
+  };
+
+  static getUserInfo(id) {
+    return fs.readFile("./src/databases/users.json")
+      .then((data) => {
+        return this.#getUserInfo(data, id);
+      })
+      .catch(console.error);
+  };
+
   static save(userInfo) {
-    const users = this.#users;
-    users.id.push(userInfo.id);
-    users.name.push(userInfo.name);
-    users.psword.push(userInfo.psword);
-    return { success: true };
+    fs.readFile("./src/databases/users.json", (err, data) => {
+      if (err) throw err;
+      const users = JSON.parse(data);
+      users.id.push(userInfo.id);
+      users.name.push(userInfo.name);
+      users.psword.push(userInfo.psword);
+      return { success: true };
+    })
   }
 };
 
