7f54177ccf82795f59a66fdb4fb556c750823921
dayforday2468 <dayforday@kaist.ac.kr>
2024-09-04 21:42:23 +0900

    fs 모듈을 이용한 회원가입 기능 구현

--- app/src/models/Users.js
+++ app/src/models/Users.js
@@ -17,9 +17,9 @@ class Users {
     }
     return { success: false, msg: "존재하지 않는 아이디입니다." }
   };
-  register() {
+  async register() {
     const client = this.body;
-    const response = UserStorage.save(client);
+    const response = await UserStorage.save(client);
     return response;
   }
 };
